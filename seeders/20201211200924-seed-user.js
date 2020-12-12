f29e93fe8c2bbb2b0053d9bf8f828a005d91bf30
Juan Carlos Moreno <jcmhsoftware@gmail.com>
2020-12-12 15:57:29 -0500

    Corregido archivo seeders

--- seeders/20201211200924-seed-user.js
+++ seeders/20201211200924-seed-user.js
@@ -1,25 +1,18 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => 
-  {
-    return queryInterface.bulkInsert('Users', 
-      [{
-        name: 'Juan',
-        email: 'jcmhsoftware@gmail.com',
-       },
+    up: async(queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('Users', [{
+            name: 'carlos',
+            email: 'ejemplo@gmail.com',
+            password: '$2y$08$FTP/jKGNASwJf0ero7SBe.kQmUsOSjWYupPZ6/lS6en6RcithXFKO',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }]);
+    },
 
-       down: async (queryInterface, Sequelize) => 
-       {
-        password: '$2y$08$ZjYFNjUHC.ubWD3sferbme7HfGqUYq9NV5kSL7CTHPMKG6fwd8diC', 
-        createdAt: new Date(), 
-        updatedAt: new Date()
-        }
-      ]);
-  },
+    down: async(queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('Users', null, {});
 
-  down: async(queryInterface, Sequelize) => 
-  {
-    return queryInterface.bulkDelete('Users', null, {});
-  }
+    }
 };
